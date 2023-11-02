6f4d1e7f83519508841fc698af3516ea3969c4d2
LuisFelipeDosSantosdaSilva <ra190874@ucdb.br>
2023-11-01 21:56:42 -0300

    Segundo commit

--- src/main/java/ucdb/AtividadeGithubBrach.java
+++ src/main/java/ucdb/AtividadeGithubBrach.java
@@ -5,6 +5,9 @@ package ucdb;
 public class AtividadeGithubBrach {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int i = 1; i <= 4; i++)
+        {
+            System.out.println("Hello World!");
+        } 
     }
 }
